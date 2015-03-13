b14733bd296c474934d738dbb2f89df767dbc597
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-03-13 20:23:14 +0100

    Add `gulp build:clean`

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 "use strict";
 
+var del = require("del");
 var gulp = require("gulp");
 var less = require("gulp-less");
 var minifyCSS = require("gulp-minify-css");
 var rename = require("gulp-rename");
 
 
-gulp.task("build", function () {
-  gulp.src("source/*.less")
-    .pipe(less())
-    .pipe(gulp.dest("dist"))
+gulp.task("build"
+  , ["build:clean"]
+  , function () {
+    return gulp.src("source/*.less")
+      .pipe(less())
+      .pipe(gulp.dest("dist"))
 
-    .pipe(minifyCSS({roundingPrecision: 3}))
-    .pipe(rename({suffix: ".min"}))
-    .pipe(gulp.dest("dist"))
-    ;
+      .pipe(minifyCSS({roundingPrecision: 3}))
+      .pipe(rename({suffix: ".min"}))
+      .pipe(gulp.dest("dist"))
+      ;
+    }
+  );
+
+gulp.task("build:clean", function (done) {
+  del("dist", done);
   });
